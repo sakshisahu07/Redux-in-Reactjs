2a3da8bf5b494cda7e6f95f5d1f89fc025f3a616
sakshisahu07 <sakshisahu072@gmail.com>
2025-03-31 13:09:13 +0530

    Redux

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "./Navbar"
+import Shop from "./Shop"
+const App = () => {
+  return (
+    <>
+   
+    <Navbar/>
+    <Shop/>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/state/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import AmountReducer from "./AmountReducer";
+
+const reducer =combineReducers({
+    amount: AmountReducer
+})
+export default reducer
